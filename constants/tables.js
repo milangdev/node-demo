5ccf1dcccee87a16095634dccd6fc4b40adb1de0
milangdev <milanghevariyadev@gmail.com>
2021-12-22 01:23:59 +0530

    query for relevant data and add line chart

---
+++ constants/tables.js
@@ -0,0 +1,7 @@
+const tables = {
+  deals: 'deals',
+  sites: 'sites',
+  dealsHistory: 'deals_history',
+};
+
+module.exports = tables;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { create } = require('express-handlebars');
+const tables = require('./constants/tables');
 const { knex } = require('./db');
 
 const PORT = process.env.PORT || 3001;
@@ -21,8 +22,47 @@ app.set('view engine', 'handlebars');
 /**
  * App routes.
  */
-app.get('/', (req, res) => {
-  res.render('home');
+app.get('/', async (req, res) => {
+  try {
+    const data = await knex(tables.sites)
+      .select([
+        `${tables.sites}.id as site_id`,
+        `${tables.sites}.title as site_title`,
+        knex.raw(`(
+          select array_agg(
+            jsonb_build_object(
+              'month', to_char(month, 'YYYY-MM'), 'avg_revenue', avg_revenue, 'deal_count', numlist
+            )
+          ) as deals_data from (
+            select date_trunc('month', d.listing_date) as month, avg(d.revenue) as avg_revenue, count(*) as numlist
+            from ${tables.deals} d
+            where d.site_id = ${knex.ref(`${tables.sites}.id`)}
+            and d.listing_date > '2020-10-31'
+            and d.listing_date < '2021-12-1'
+            group by date_trunc('month', d.listing_date)
+          ) deals
+        )`),
+      ])
+      .leftJoin(
+        tables.deals,
+        `${tables.sites}.id`,
+        `${tables.deals}.site_id`,
+      )
+      .where((builder) => {
+        builder
+          .where(`${tables.deals}.listing_date`, '>', '2020-10-31')
+          .andWhere(`${tables.deals}.listing_date`, '<', '2021-12-1');
+      })
+      .orderBy(`${tables.sites}.id`)
+      .groupBy(`${tables.sites}.id`);
+
+    res.render('home', {
+      data: JSON.stringify(data),
+    }); 
+  } catch (error) {
+    // TODO - send error response or render error page.
+    console.error(error); 
+  }
 });
 
 
@@ -37,4 +77,3 @@ knex.raw('select 1+1 as result').then(() => {
   console.log(err);
   process.exit(1);
 });
-
